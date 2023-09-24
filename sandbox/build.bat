dcd69e03cbb474eff44dc9a14c459548d95c1b3b
Thunder <robert.shalders@hotmail.com>
2023-09-24 17:55:40 +0100

    [+] Added build script for sandbox

---
+++ sandbox/build.bat
@@ -0,0 +1,20 @@
+REM Build script for sandbox
+@ECHO OFF
+SetLocal EnableDelayedExpansion
+
+REM Retrieve source files
+Set sourceFiles=
+FOR /R %%f in (*.c) do (
+    SET sourceFiles=!sourceFiles! %%f
+)
+
+REM echo "Source Files:" %sourceFiles%
+
+SET assembly=sandbox
+SET compilerFlags=-g
+SET includeFlags=-Isrc -I../engine/src
+SET linkerFlags=-l../bin/ -lengine.lib
+SET defines=-D_DEBUG -DKIMPORT
+
+ECHO "Building %assembly%..."
+clang %sourceFiles% %compilerFlags% -o ../bin/%assembly%.exe %defines% %includeFlags% %linkerFlags%
