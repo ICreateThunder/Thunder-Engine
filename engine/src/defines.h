455fc11e937b29edaff57e63019a217b34beda73
Thunder <robert.shalders@hotmail.com>
2023-09-24 17:11:10 +0100

    [+] Added type shortening and platform detection and macro setting

---
+++ engine/src/defines.h
@@ -0,0 +1,88 @@
+#pramga once
+
+// Shorten the names of type to [first letter of type][number of bytes]
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned int u32;
+typedef unsigned long long u64;
+
+typedef signed char i8;
+typedef signed short i16;
+typedef signed int i32;
+typedef signed long long i64;
+
+typedef float f32;
+typedef double f64;
+
+typedef int b32;
+typedef char b8;
+
+// Test to ensure types have correct size
+
+#if defined(__clang__) || defined(__gcc__)
+#define STATIC_ASSERT _Static_assert
+#else
+#define STATIC_ASSERT static_assert
+#endif
+
+STATIC_ASSERT(sizeof(u8) == 1, "Expected u8 to be 1 byte");
+STATIC_ASSERT(sizeof(u16) == 2, "Expected u16 to be 2 bytes");
+STATIC_ASSERT(sizeof(u32) == 4, "Expected u32 to be 4 bytes");
+STATIC_ASSERT(sizeof(u64) == 8, "Expected u64 to be 8 bytes");
+
+STATIC_ASSERT(sizeof(i8) == 1, "Expected i8 to be 1 bytes");
+STATIC_ASSERT(sizeof(i16) == 2, "Expected i16 to be 2 bytes");
+STATIC_ASSERT(sizeof(i32) == 4, "Expected i32 to be 4 bytes");
+STATIC_ASSERT(sizeof(i64) == 8, "Expected i64 to be 8 bytes");
+
+STATIC_ASSERT(sizeof(f32) == 4, "Expected f32 to be 4 bytes");
+STATIC_ASSERT(sizeof(f64) == 8, "Expected f64 to be 8 bytes");
+
+#define TRUE 1
+#define FALSE 0
+
+// Platform detection
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+#define KPLATFORM_WINDOWS 1
+#ifndef _WIN64
+#error "64-bit is required on Windows!"
+#endif
+#elif defined(__linux__) || defined(__gnu_linux__)
+#define KPLATFORM_LINUX 1
+#if defined(__ANDROID__)
+#define KPLATFORM_ANDROID 1
+#endif
+#elif defined(__unix__)
+#define KPLATFORM_UNIX 1
+#elif defined(_POSIX_VERSION)
+#define KPLATFORM_POSIX 1
+#elif __APPLE__
+#define KPLATFORM_APPLE 1
+#include <TargetConditionals.h>
+#if TARGET_IPHONE_SIMULATOR
+#define KPLATFORM_IOS 1
+#define KPLATFORM_IOS_SIMULATOR 1
+#elif TAGET_OS_IPHONE
+#define KPLATFORM_IOS 1
+#elif TARGET_OS_MAX
+#else
+#error "Unknown Apple platform"
+#endif
+#else
+#error "Unknown platform!"
+#endif
+
+#ifndef KEXPORT
+#ifdef _MSC_VER
+#define KAPI __declspec(dllexport)
+#else
+#define KAPI __attribute__((visibility("default")))
+#endif
+#else
+#ifndef _MSC_VER
+#define KAPI __declspec(dllimport)
+#else
+#define KAPI
+#endif
+#endif
