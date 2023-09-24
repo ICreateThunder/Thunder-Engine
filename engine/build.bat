e3f60b0139d94848745c8d86a3ee947e6684a12a
Thunder <robert.shalders@hotmail.com>
2023-09-24 16:05:23 +0100

    [+] Added build script for Windows

---
+++ engine/build.bat
@@ -0,0 +1,21 @@
+REM Build script for engine
+@ECHO OFF
+SetLocal EnableDelayedExpansion
+
+REM Retrieve source files
+Set sourceFiles=
+FOR /R %%f in (*.c) do (
+    SET sourceFiles=!sourceFiles! %%f
+)
+
+REM echo "Source Files:" %sourceFiles%
+
+SET assembly=engine
+SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
+REM -Wall -Werror
+SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
+SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
+SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
+
+ECHO "Building %assembly%..."
+clang %sourceFiles% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
