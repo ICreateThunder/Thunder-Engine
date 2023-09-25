fad04c67d858015ee99010617f1c1791512c6eab
ICreateThunder <iamrobshalr@gmail.com>
2023-09-25 19:15:03 +0100

    [+] Added build all script for linux

---
+++ build-all.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set echo on
+
+echo "Building everything..."
+
+pushd engine
+source build.sh
+popd
+
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]
+then
+echo "Error:"$ERRORLEVEL && exit
+fi
+
+pushd sandbox
+source build.sh
+popd
+
+ERRORLEVEL=$?
+if [ $ERRORLEVEL -ne 0 ]
+then
+echo "Error:"$ERRORLEVEL && exit
+fi
+
+echo "All assemblies built successfully"
