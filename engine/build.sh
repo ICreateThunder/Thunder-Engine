fc65e61f56f3a1bc1b6b050af211f2e43c1da167
ICreateThunder <iamrobshalr@gmail.com>
2023-09-25 18:50:00 +0100

    [+] Added engine build script for linux

---
+++ engine/build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo echo on
+
+mkdir -p ../bin
+
+sourceFiles=$(find . -type f -name "*.c")
+
+assembly="engine"
+compilerFlags="-g -shared -fdeclspec -fPIC"
+includeFlags="=Isrc -I$VULKAN_SDK/include"
+linkerFlags="-lvulkan -lxcb -lX11 -lX11-xcb -lxkbcommon -L$VULKAN_SDK/lib -L/usr/X11R6/lib"
+defines="-D_DEBUG -DKEXPORT"
+
+echo "Building $assembly
+clan $sourceFiles $compilerFlags -o ../bin/lib$assembly.so $defines $includeFlags $linkerFlags
