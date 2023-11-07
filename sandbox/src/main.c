901d6211b3404801182d7d431370ae6554032535
ICreateThunder <iamrobshalr@gmail.com>
2023-11-07 14:32:54 +0000

    [-] Updated main to reflect removal of platform level code/interface

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,22 +1,20 @@
 #include <core/logger.h>
 #include <core/asserts.h>
-#include <platform/platform.h>
+
+#include <core/application.h>
 
 int main(void) {
-    KFATAL("Something went wrong: %f", 3.14f);
-    KERROR("Something went wrong: %f", 3.14f)
-    KWARN("Something went wrong: %f", 3.14f);
-    KINFO("Something went wrong: %f", 3.14f);
-    KDEBUG("Something went wrong: %f", 3.14f);
-    KTRACE("Something went wrong: %f", 3.14f);
 
-    platform_state state;
-    if (platform_startup(&state, "Thunder Engine Sandbox", 100, 100, 1280, 720)) {
-        while(TRUE) {
-            platform_pump_messages(&state);
-        }
-    }
-    platform_shutdown(&state);
+    application_config config;
+    config.start_x_pos = 100;
+    config.start_y_pos = 100;
+    config.start_width = 1280;
+    config.start_height = 720;
+    config.name = "Thunder's Engine Sandbox";
+
+    application_create(&config);
+    application_run();
+    
 
     return 0;
 }
