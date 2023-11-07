e86d973db5d9b47b4f50c495ecc6d9b342ebe9a1
ICreateThunder <iamrobshalr@gmail.com>
2023-11-07 14:09:34 +0000

    [+] Added initial application functionality/interface

---
+++ engine/src/core/application.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "defines.h"
+
+typedef struct application_config {
+    i16 start_x_pos;
+    i16 start_y_pos;
+    i16 start_width;
+    i16 start_height;
+    char* name;
+} application_config;
+
+KAPI b8 application_create();
+
+KAPI b8 application_run();
