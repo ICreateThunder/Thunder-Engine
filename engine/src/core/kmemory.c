ad20a65cedeb2fa9484e202aafed5c7f9aa3a4e1
ICreateThunder <iamrobshalr@gmail.com>
2023-11-14 16:30:44 +0000

    [-] Updated string.h function call for windows

--- engine/src/core/kmemory.c
+++ engine/src/core/kmemory.c
@@ -110,6 +110,6 @@ char* get_memory_usage_str() {
         i32 length = snprintf(buffer + offset, 8000, "  %s: %.2f%s\n", memory_tag_strings[i], amount, unit);
         offset += length;
     }
-    char* out_string = strdup(buffer);
+    char* out_string = _strdup(buffer);
     return out_string;
 }
